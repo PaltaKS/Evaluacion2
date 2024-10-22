fb3b14e0c9b189a2b3750f39cd7bb872da8619ba
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 04:17:57 -0300

    merge

--- app/src/main/java/com/example/evaluacion_2/Parent_Device.java
+++ app/src/main/java/com/example/evaluacion_2/Parent_Device.java
@@ -17,6 +17,7 @@ public class Parent_Device extends AppCompatActivity {
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parent_device);
