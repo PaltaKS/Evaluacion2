7c7f54d4ce6127b8ce76e2b2cf2776f638a9a8b0
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 02:02:22 -0300

    Funcionalidad Botones Click 'register and close'

--- app/src/main/java/com/example/evaluacion_2/Register.java
+++ app/src/main/java/com/example/evaluacion_2/Register.java
@@ -1,6 +1,9 @@
 package com.example.evaluacion_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,22 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
+        Button BtnRegis = findViewById(R.id.registerBtn);
+        Button BtnClose = findViewById(R.id.closeBtn);
 
+        BtnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Register.this, "Registro Con Exito", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Register.this, "Cerrar Registro", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
