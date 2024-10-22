ffe8de86fc30d7606a5c24536526aed88c3f9c6b
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 03:35:49 -0300

    funcionalidad completa Inicio, se agregan pantallas Cofig_Device, Control_Device, Profile

--- app/src/main/java/com/example/evaluacion_2/Inicio.java
+++ app/src/main/java/com/example/evaluacion_2/Inicio.java
@@ -12,7 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Inicio extends AppCompatActivity {
 
-    private Button closeSesion;
+    private Button closeSesion, BtnConfDevice, BtnControlDevice, BtnProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +23,39 @@ public class Inicio extends AppCompatActivity {
         // Catch ID XML to Inicio
 
         closeSesion = findViewById(R.id.CloseButton);
+        BtnConfDevice = findViewById(R.id.BtnConfDevice);
+        BtnControlDevice = findViewById(R.id.BtnControlDevices);
+        BtnProfile = findViewById(R.id.BtnProfile);
 
         //Funcionalidad Button Emperejar Dispositivo
 
-
-
-
-
+        BtnConfDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ConfIntent = new Intent(Inicio.this, Config_Device.class );
+                startActivity(ConfIntent);
+            }
+        });
 
         //Funcionalidad Button Control Dispositivo
 
+        BtnControlDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ControlIntent = new Intent(Inicio.this, Control_Device.class);
+                startActivity(ControlIntent);
+            }
+        });
 
+        //Funcionalidad Button Ver Perfil
 
-
+        BtnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ProfileIntent = new Intent(Inicio.this, Profile.class);
+                startActivity(ProfileIntent);
+            }
+        });
 
         //Funcionalidad Button Cerrar
 
@@ -48,8 +69,5 @@ public class Inicio extends AppCompatActivity {
                 finish();
             }
         });
-
-
-
     }
 }
