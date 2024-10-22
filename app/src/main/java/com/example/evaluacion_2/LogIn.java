b04ef449b10bc3d53e08b8afa2253ea35f4f2324
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 00:44:33 -0300

    Modificacion y creacion de LogIn

---
+++ app/src/main/java/com/example/evaluacion_2/LogIn.java
@@ -0,0 +1,19 @@
+package com.example.evaluacion_2;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+
+
+
+    }
+}
