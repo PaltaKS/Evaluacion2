c3ccc4c4ece0a5bfad8d2b613d9f581eb08e6da1
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 00:56:32 -0300

    Implementacion Boton LogIn, Creacion de Pantalla Inicio

--- app/src/main/java/com/example/evaluacion_2/LogIn.java
+++ app/src/main/java/com/example/evaluacion_2/LogIn.java
@@ -1,6 +1,11 @@
 package com.example.evaluacion_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +13,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LogIn extends AppCompatActivity {
 
+    //Declaracion de Variables
+    private EditText IdUser;
+    private EditText IdPass;
+    private Button SessionInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
+        IdUser = findViewById(R.id.EditTextUserName);
+        IdPass = findViewById(R.id.EditTextPassword);
+        Button IdBtnIn = findViewById(R.id.SessionInButton);
+
+        //Configurar Listener para el Boton
+
+        IdBtnIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = IdUser.getText().toString();
+                String userpass = IdPass.getText().toString();
 
 
+                //Mostrar Valorares Alerta
+
+                //Toast.makeText(logInActivity.this, "Usuario: " + username, Toast.LENGTH_LONG).show();
+
+                // Validar si el User es admin
+                // Validar si la Pass es adminadmin
+                // Si los valores Coinsiden redirigir al Home o Pagina Pricipal
+                // Si los valores no coinsiden mostrar Alert si los valores no coinciden
+
+                if(username.equals("admin") && userpass.equals("adminadmin")){
+                    // Alert Log In
+                    Toast.makeText(LogIn.this, "Log In" , Toast.LENGTH_LONG).show();
+
+                    Intent logIn = new Intent(LogIn.this, Inicio.class);
+                    startActivity(logIn);
+                }
+                else{
+                    //
+                    Toast.makeText(LogIn.this, "Usuario o Contrasena Incorrecto" , Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
