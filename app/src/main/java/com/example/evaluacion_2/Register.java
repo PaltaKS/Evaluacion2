25080b851787b960015f35d34d1fefc0b11da5c4
Bastian Pavez <bpavez03@gmail.com>
2024-10-22 01:36:40 -0300

    rename XML registrar to register

--- app/src/main/java/com/example/evaluacion_2/Register.java
+++ app/src/main/java/com/example/evaluacion_2/Register.java
@@ -11,7 +11,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrar);
+        setContentView(R.layout.register);
 
 
 
